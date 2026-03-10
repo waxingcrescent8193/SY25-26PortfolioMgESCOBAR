2fbd475c1e6b5299b69609fe5569e9b6fc1fee57
waxingcrescent8193 <b2029jezescobar@pshs.edu.ph>
2026-03-10 21:02:27 +0800

    q4g2 U of CRUD

--- public/q4/1stGraded.js
+++ public/q4/1stGraded.js
@@ -8,9 +8,9 @@ form.addEventListener("submit", function(e){
     e.preventDefault();
 
     if(confirm("Save your response?")) {
-        const movieTitle = document.getElementById("title").value;
-        const yearOfRelease = document.getElementById("year").value;
-        const genre = document.getElementById("genre").value;
+        const movieTitle = document.getElementById("title").value.trim();
+        const yearOfRelease = document.getElementById("year").value.trim();
+        const genre = document.getElementById("genre").value.trim();
 
         let ratingSelection = document.getElementsByName("rating");
         let rating;
@@ -25,7 +25,7 @@ form.addEventListener("submit", function(e){
         let movieRated = {
             title: movieTitle,
             yearReleased: yearOfRelease,
-            genre: genre,
+            genre: [genre],
             rating: [rating]
         };
 
@@ -36,16 +36,23 @@ form.addEventListener("submit", function(e){
         //counterchecking if movieRated is alr in local storage and then just update
 
         for (let i in listOfMovies) {
-            if(listOfMovies[i].title == movieRated.title) {
+            if(listOfMovies[i].title == movieRated.title && listOfMovies[i].yearReleased == movieRated.yearReleased) {
                 tracker = true;
+                let genreTrack = false;
 
-                if(listOfMovies[i].genre != movieRated.genre)
-                    listOfMovies[i].genre += ", " + movieRated.genre; 
-
-                // ratings
+                for(let k in listOfMovies[i].genre) {
+                    if(listOfMovies[i].genre[k] == movieRated.genre)
+                        genreTrack = true;
+                }
+                
+                if(!genreTrack)
+                    listOfMovies[i].genre.push(genre);
+                
+                listOfMovies[i].rating.push(rating);
 
             }
         }
+        
 
         if(!tracker)
             listOfMovies.push(movieRated);
@@ -71,6 +78,8 @@ function unfocused (element) {
 }
 
 function displayMovies(movies) {
+    submittedMovies.innerHTHML = "";
+    
     for(let i in movies) {
         let movieDisplay = document.createElement("div");
         let display = document.createElement("p");
@@ -78,17 +87,35 @@ function displayMovies(movies) {
         let ratingString = document.createElement("p");
         let star = "&#9733;";
 
+        let aveRating = 0;
+        let genreString = "";
+
         movieDisplay.classList.add("movieDisplay");
 
-        for(let j=0; j < movies[i].rating; j++) {
+        for(let k=0; k < movies[i].rating.length; k++) {
+            aveRating += parseInt(movies[i].rating[k]);
+        }
+
+        aveRating /= (movies[i].rating.length);
+
+        console.log(aveRating);
+
+        aveRating = Math.ceil(aveRating);
+
+        for(let j=0; j < aveRating; j++) {
             ratingString.innerHTML += star;
+        }
+
+        //genre string
+        for(let m in movies[i].genre) {
+            genreString += movies[i].genre[m] + ", "
         }
 
         ratingDisplay.appendChild(ratingString);
 
         ratingString.classList.add("ratingStars");
 
-        display.innerHTML = `${movies[i].title} (${movies[i].yearReleased}) - ${movies[i].genre}, Rating: `;
+        display.innerHTML = `${movies[i].title} (${movies[i].yearReleased}) - ${genreString} Rating: `;
 
         movieDisplay.appendChild(display);
         movieDisplay.appendChild(ratingDisplay);
